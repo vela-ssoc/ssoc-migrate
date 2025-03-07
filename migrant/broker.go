62542f690156a59ef7a4af547aa94c656593f9a6
xmx <xmx@github.com>
2025-03-07 15:06:46 +0800

    更新依赖

---
+++ migrant/broker.go
@@ -0,0 +1,32 @@
+package migrant
+
+import (
+	"context"
+	"log/slog"
+
+	"github.com/vela-ssoc/vela-common-mb/dal/model"
+	"github.com/vela-ssoc/vela-common-mb/dal/query"
+)
+
+func NewBroker(from *query.Query, dest *query.Query, log *slog.Logger) *Broker {
+	return &Broker{from: from, dest: dest, log: log}
+}
+
+type Broker struct {
+	from *query.Query
+	dest *query.Query
+	log  *slog.Logger
+}
+
+func (*Broker) TableName() string {
+	return new(model.Broker).TableName()
+}
+
+func (mig *Broker) Execute(ctx context.Context) error {
+	dats, err := mig.from.Broker.WithContext(ctx).Find()
+	if err != nil {
+		return err
+	}
+
+	return mig.dest.Broker.WithContext(ctx).Create(dats...)
+}
